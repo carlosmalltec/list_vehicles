a84a0ea49b854fad79abb3ab46eed78b2a443b52
Carlos Santos <francisco.santos@olistpax.com>
2022-03-31 13:37:03 -0300

    redicionar splash para home

--- lib/modules/splash/bindings/splash_bindings.dart
+++ lib/modules/splash/bindings/splash_bindings.dart
@@ -1,9 +1,9 @@
+import 'package:appvehicles/modules/splash/presenter/controller/splash_controller.dart';
 import 'package:get/get.dart';
 
 class SplashBindings implements Bindings {
   @override
   void dependencies() {
-    // Get.put(SharedPreferencesHelper());
-    
+    Get.put(SplashController());
   }
 }
